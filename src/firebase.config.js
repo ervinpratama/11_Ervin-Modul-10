88aa7e2b519b998d3e8ea130c7bab98581efb46a
vinsifa <vinpratama12@gmail.com>
2022-04-24 09:14:43 +0700

    Modul 10

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBZ9VbID4hcKD7jsnpN2BZw7I50qvSaLLM",
+     authDomain: "pbf-modul-10-7bb96.firebaseapp.com",
+     projectId: "pbf-modul-10-7bb96",
+     storageBucket: "pbf-modul-10-7bb96.appspot.com",
+     messagingSenderId: "810874505282",
+     appId: "1:810874505282:web:043bdc083f91f65f3045db",
+     measurementId: "G-B1S198RFW4"
+}
